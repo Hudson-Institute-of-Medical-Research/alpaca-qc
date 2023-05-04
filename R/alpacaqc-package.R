3c2014d4cf0df3ccd5ad3ed4acc4632ad3379527
SpikyClip <74215773+SpikyClip@users.noreply.github.com>
2023-05-04 16:19:01 +1000

    check().

--- R/alpacaqc-package.R
+++ R/alpacaqc-package.R
@@ -2,15 +2,19 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @import checkmate
+#' @import cli
 #' @import dplyr
 #' @import lubridate
 #' @import stringr
 #' @import tidyr
+#' @import validate
 #' @importFrom glue glue
 #' @importFrom lubridate dmy_hms
 #' @importFrom meltr melt_csv
 #' @importFrom purrr pluck
 #' @importFrom readr read_csv
+#' @importFrom rlang .data
 #' @importFrom tibble add_column
 #' @importFrom tibble as_tibble
 #' @importFrom tidyr pivot_wider
